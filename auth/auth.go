a0dc06e7b9779ebdffca288fc7bdb8378d70d9d8
Antonio Calabrese <ancalabrese@google.com>
2024-04-13 20:46:11 +0100

    Add text for user input

--- auth/auth.go
+++ auth/auth.go
@@ -1,11 +1,28 @@
 package auth
 
 import (
-	"net/http"
+	"encoding/base64"
 
 	"github.com/ancalabrese/mc-cli/mc"
+	"golang.org/x/oauth2"
 )
 
-func RequestAuthorizationCode(c *mc.Config) (string, error) {
-	//todo: do
+type AuthClient struct {
+	oauth2.Config
+}
+
+func NewAuthClient(c *mc.Config) *AuthClient {
+	config := &AuthClient{}
+	endpoint := oauth2.Endpoint{}
+
+	endpoint := oauth2.Endpoint{}
+	config.Endpoint.DeviceAuthURL = c.DetectHost()
+	config.ClientID = c.DetectClientId()
+	config.ClientSecret = c.DetectClientSecret()
+	config.RedirectURL = c.DetectCallbackURI()
+	return config
+}
+
+func getAuthorizationCodeRequestHeader(clientId string, secret string) string {
+	return base64.RawStdEncoding.EncodeToString([]byte(clientId + ":" + secret))
 }
--- mc/config.go
+++ mc/config.go
@@ -17,6 +17,17 @@ type Config struct {
 	stdinScanner *bufio.Scanner
 }
 
+func NewConfig() Config {
+	c := Config{}
+
+	c.PromptForHostname()
+	c.PromptForClientId()
+	c.PromptForSecret()
+	c.PromptForCallbackUri()
+
+	return c
+}
+
 func (c *Config) DetectApiToken() string {
 	return os.Getenv(mcTokenKey)
 }
@@ -42,6 +53,7 @@ func (c *Config) PromptForHostname() string {
 	if host != "" {
 		return host
 	}
+	println("Mobicontrol server hast name:")
 
 	//TODO: sanitize
 	host = c.scanLine()
@@ -54,6 +66,8 @@ func (c *Config) PromptForClientId() string {
 	if clientId != "" {
 		return clientId
 	}
+	println("Mobicontrol API client ID:")
+
 	//TODO: sanitize
 	clientId = c.scanLine()
 
@@ -65,11 +79,25 @@ func (c *Config) PromptForSecret() string {
 	if secret != "" {
 		return secret
 	}
+	println("Mobicontrol API secret:")
 
 	//TODO: sanitize
 	secret = c.scanLine()
 
 	return secret
+}
+
+func (c *Config) PromptForCallbackUri() string {
+	url := c.DetectCallbackURI()
+	if url != "" {
+		return url
+	}
+	println("Mobicontrol API client callback URI (default: mcauth://callback):")
+
+	//TODO: sanitize
+	url = c.scanLine()
+
+	return url
 }
 
 func (c *Config) scanLine() string {
