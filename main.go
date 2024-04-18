ac136ff93a043fc4e1754a2e0e7ebc0fecef13ff
Antonio Calabrese <ancalabrese@google.com>
2024-04-18 20:24:28 +0100

    Wait for kill signal before terminating main proc

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+
 	"github.com/ancalabrese/mc-cli/mc/auth"
 	"github.com/ancalabrese/mc-cli/mc/config"
 )
@@ -17,4 +20,10 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	_ = <-sigChan
 }
