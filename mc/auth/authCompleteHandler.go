d6abb5b2b7131d7ea4c413c30059615579218245
Antonio Calabrese <ancalabrese@google.com>
2024-04-26 07:47:12 +0100

    mend

--- mc/auth/authCompleteHandler.go
+++ mc/auth/authCompleteHandler.go
@@ -10,7 +10,7 @@ const htmlPagePath = "./mc/auth/authComplete.html"
 func (as *AuthSession) AuthCompleteHandler(w http.ResponseWriter, r *http.Request) {
 	//TODO: check that this was a redirection from token handler.
 	if as.Token == nil {
-		as.Logger.Error("AuthCompleteHandler: user got to complete without a valid token")
+		as.Logger.Error("AuthCompleteHandler: user got to complete without a valid access token")
 		http.Error(w, "Invalid access token", http.StatusBadRequest)
 		return
 	}
