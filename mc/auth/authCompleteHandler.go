89605c88d2c2ac67bfb59b85e2cb6f7c8e91eb71
Antonio Calabrese <ancalabrese@google.com>
2024-04-26 07:32:45 +0100

    Fix auth complete html file path

--- mc/auth/authCompleteHandler.go
+++ mc/auth/authCompleteHandler.go
@@ -5,12 +5,13 @@ import (
 	"os"
 )
 
-const htmlPagePath = "./authComplete.html"
+const htmlPagePath = "./mc/auth/authComplete.html"
 
 func (as *AuthSession) AuthCompleteHandler(w http.ResponseWriter, r *http.Request) {
 	//TODO: check that this was a redirection from token handler.
 	pageData, err := os.ReadFile(htmlPagePath)
 	if err != nil {
+		as.Logger.Debug("AuthCompleteHandler", "err", err)
 		w.Write([]byte("Authentication complete. You may now close this page."))
 	}
 	w.Write(pageData)
