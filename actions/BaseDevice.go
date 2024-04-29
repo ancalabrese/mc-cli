9c0f5600b2f39da0f547fe0ad958c36dea1fd348
Antonio Calabrese <ancalabrese@google.com>
2024-04-29 18:11:05 +0100

    Add base getDevice implementation

--- actions/BaseDevice.go
+++ actions/BaseDevice.go
@@ -1,5 +1,13 @@
 package actions
 
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/ancalabrese/mc-cli/utils"
+)
+
 type BaseDevice struct {
 	Kind                   DeviceType             `json:"Kind"`
 	CompliancePolicyStatus CompliancePolicyStatus `json:"CompliancePolicyStatusType"`
@@ -22,4 +30,29 @@ type BaseDevice struct {
 	ServerName             string                 `json:"ServerName"`
 	Platform               PlatformType           `json:"Platform"`
 	Manufacturer           string                 `json:"Manufacturer"`
+}
+
+const endpoint = "devices"
+
+func GetDevices(ctx context.Context, client http.Client) ([]BaseDevice, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "", nil)
+	utils.Check(err)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	b := make([]byte, resp.ContentLength)
+	_, err = resp.Body.Read(b)
+	if err != nil {
+		return nil, err
+	}
+	devices := make([]BaseDevice, 0)
+	json.Unmarshal(b, &devices)
+
+	return devices, nil
+}
+
+func GetDeviceById(client http.Client) ([]BaseDevice, error) {
+	return nil, nil
 }
