d16faac334a1b68221a2753c7b1daf00bb115851
Antonio Calabrese <ancalabrese@google.com>
2024-05-03 16:37:10 +0100

    Add functions to construct McClient

--- mc/client/mcClient.go
+++ mc/client/mcClient.go
@@ -7,23 +7,45 @@ import (
 )
 
 type McClient struct {
-	Host           string
-	DisplayVersion string
-	Version        int64
-	HttpClient     http.Client
+	Host               string
+	DisplayVersion     string
+	Version            int64
+	HttpClient         *http.Client
+	ApiBaseAddress     string
+	DevicesApiEndpoint func() string
 }
 
 const mcPath = "Mobicontrol"
 const apiPath = "api"
+const MC_V14 = 14
+const DEVICES_API_PATH = "devices"
+const DEVICES_SEARCH_PATH = "search"
 
-func NewMcClient(host string) (*McClient, error) {
+func NewMcClient(host string, c *http.Client) (*McClient, error) {
 	if !isValidHostName(host) {
 		return nil, fmt.Errorf("Couldn't create a valid client for MC: Invalid host name %s", host)
 	}
 
 	return &McClient{
-		Host: host,
+		Host:           host,
+		HttpClient:     c,
+		ApiBaseAddress: getApiAddress(host),
 	}, nil
+}
+
+func (mcc *McClient) getDevicesApiEndpoint() string {
+	u, _ := url.Parse(mcc.ApiBaseAddress)
+
+	if mcc.Version == 0 || mcc.Version < MC_V14 {
+		return u.JoinPath(DEVICES_API_PATH).String()
+	}
+
+	return u.JoinPath(DEVICES_SEARCH_PATH).String()
+}
+
+func getApiAddress(host string) string {
+	u, _ := url.Parse("https://" + host)
+	return u.JoinPath(mcPath, apiPath).String()
 }
 
 func isValidHostName(h string) bool {
