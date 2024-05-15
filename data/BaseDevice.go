0eecc7e1cea595b63f50559de1bd5572a7317ca6
Antonio Calabrese <ancalabrese@google.com>
2024-05-15 07:49:08 +0100

    Tag base device with prettyPrint

--- data/BaseDevice.go
+++ data/BaseDevice.go
@@ -1,25 +1,25 @@
 package data
 
 type BaseDevice struct {
-	Kind                   DeviceType             `json:"Kind"`
-	CompliancePolicyStatus CompliancePolicyStatus `json:"CompliancePolicyStatusType"`
-	ComplianceStatus       bool                   `json:"ComplianceStatus"`
-	ComplianceItems        []ComplianceItem       `json:"ComplianceItems"`
-	DeviceId               string                 `json:"DeviceId"`
-	DeviceName             string                 `json:"DeviceName"`
-	EnrollmentType         DeviceEnrollmentType   `json:"EnrollmentType"`
-	EnrollmentTime         string                 `json:"EnrollmentTime"`
-	Family                 DeviceFamilyType       `json:"Family"`
-	HostName               string                 `json:"HostName"`
-	IsAgentOnline          bool                   `json:"IsAgentOnline"`
-	MacAddress             string                 `json:"MacAddress"`
-	BluetoothMAC           string                 `json:"BluetoothMAC"`
-	WifiMAC                string                 `json:"WifiMAC"`
-	Mode                   DeviceMode             `json:"Mode"`
-	Model                  string                 `json:"Model"`
-	OsVersion              string                 `json:"OsVersion"`
-	Path                   string                 `json:"Path"`
-	ServerName             string                 `json:"ServerName"`
-	Platform               PlatformType           `json:"Platform"`
-	Manufacturer           string                 `json:"Manufacturer"`
+	Kind                   DeviceType             `json:"Kind" prettyPrint:"Kind"`
+	CompliancePolicyStatus CompliancePolicyStatus `json:"CompliancePolicyStatusType" prettyPrint:"CompliancePolicyStatus"`
+	ComplianceStatus       bool                   `json:"ComplianceStatus" prettyPrint:"ComplianceStatus"`
+	ComplianceItems        []ComplianceItem       `json:"ComplianceItems" prettyPrint:"ComplianceItems"`
+	DeviceId               string                 `json:"DeviceId" prettyPrint:"DeviceId"`
+	DeviceName             string                 `json:"DeviceName" prettyPrint:"DeviceName"`
+	EnrollmentType         DeviceEnrollmentType   `json:"EnrollmentType" prettyPrint:"EnrollmentType"`
+	EnrollmentTime         string                 `json:"EnrollmentTime" prettyPrint:"EnrollmentTime"`
+	Family                 DeviceFamilyType       `json:"Family" prettyPrint:"Family"`
+	HostName               string                 `json:"HostName" prettyPrint:"HostName"`
+	IsAgentOnline          bool                   `json:"IsAgentOnline" prettyPrint:"IsAgentOnline"`
+	MacAddress             string                 `json:"MacAddress" prettyPrint:"MacAddress"`
+	BluetoothMAC           string                 `json:"BluetoothMAC" prettyPrint:"BluetoothMAC"`
+	WifiMAC                string                 `json:"WifiMAC" prettyPrint:"WifiMAC"`
+	Mode                   DeviceMode             `json:"Mode" prettyPrint:"Mode"`
+	Model                  string                 `json:"Model" prettyPrint:"Model"`
+	OsVersion              string                 `json:"OsVersion" prettyPrint:"OsVersion"`
+	Path                   string                 `json:"Path" prettyPrint:"Path"`
+	ServerName             string                 `json:"ServerName" prettyPrint:"ServerName"`
+	Platform               PlatformType           `json:"Platform" prettyPrint:"Platform"`
+	Manufacturer           string                 `json:"Manufacturer" prettyPrint:"Manufacturer"`
 }
